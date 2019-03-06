f6edafe67e0cb559dcbb891e02d8e69bfe744ffd
alice-mu <alicemu99@gmail.com>
2019-03-06 13:33:41 +0800

    finds green light alright, loads and displays video alright

--- TestFindLight/TestFindLight.cpp
+++ TestFindLight/TestFindLight.cpp
@@ -1,21 +1,89 @@
-// TestFindLight.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
+#include <opencv2/opencv.hpp>  
+#include <opencv/cv.h>  
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+#include <opencv2/ml/ml.hpp>  
+
+using namespace std;
+using namespace cv;
+using namespace cv::ml;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	VideoCapture video("dimmer.mov");
+	if (!video.isOpened()) {
+		cerr << "Error opening video stream or file" << endl;
+		return -1;
+	}
+
+	vector<Mat> contours;
+	// Process frame-by-frame
+	while (1) {
+		Mat img;
+		video >> img;
+		if (img.empty())
+			break;
+
+		//Mat img = imread("normal.jpg");
+		Mat img2;
+		img.copyTo(img2);
+
+		cvtColor(img2, img2, COLOR_RGB2HSV);
+		int minhueG = 40; int maxhueG = 75;
+		int minsatG = 60; int maxsatG = 255;
+		int minvalG = 80; int maxvalG = 255;
+
+		Mat colourFound;
+		inRange(img2, Scalar(minhueG, minsatG, minvalG), Scalar(maxhueG, maxsatG, maxvalG), colourFound);
+		Mat dilateElement = getStructuringElement(MORPH_ELLIPSE, Size(2, 2));
+		dilate(colourFound, colourFound, dilateElement, Point(-1, -1), 3);
+
+		Mat colourFound2;
+		bitwise_and(img2, img2, colourFound2, colourFound);
+
+		findContours(colourFound, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+
+		vector<Rect> boundRects(contours.size());
+		for (int i = 0; i < contours.size(); i++) {
+			boundRects[i] = boundingRect(contours.at(i));
+		}
+
+
+		vector<Rect>::iterator currRect = boundRects.begin();
+		while (currRect != boundRects.end()) {
+			int w = currRect->width;
+			int h = currRect->height;
+
+			if (abs(w - h) > 0.25 * min(w, h) || w * h < img.cols*img.rows / 20000)
+				currRect = boundRects.erase(currRect);
+			else
+				currRect++;
+		}
+
+
+		Mat colourFound3;
+		colourFound2.copyTo(colourFound3);
+		for (int i = 0; i < boundRects.size(); i++)
+		{
+			rectangle(img, boundRects[i].tl(), boundRects[i].br(), Scalar(0, 255, 255), 2, 8, 0);
+		}
+
+		imshow("filtered.jpg", img);
+		
+		// Press ESC on keyboard to exit
+		char c = (char)waitKey(1);
+		if (c == ' ')
+			waitKey();
+		if (c == 27)
+			break;
+	}
+
+	// When everything done, release the video capture object
+	video.release();
+	// Closes all the frames
+	destroyAllWindows();
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
